df6ade6e69b3bab01ccf3bbbd8815c90824c37e6
Winston Weinert <winston@ml1.net>
2020-09-04 10:02:44 -0500

    admin: allow for expiry to be blank
    https://stackoverflow.com/questions/11351619/how-to-make-djangos-datetimefield-optional/11351690

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ class Paste(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    expiry = models.DateTimeField(null=True)
+    expiry = models.DateTimeField(null=True, blank=True)
     hits = models.PositiveIntegerField(default=0)
 
     def view(self):
