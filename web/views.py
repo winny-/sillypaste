b783cf42a06ac47ac47b9937ed5953756ea02a5f
Winston Weinert <winston@ml1.net>
2020-09-05 13:35:46 -0500

    small style changes

--- web/views.py
+++ web/views.py
@@ -18,20 +18,26 @@ def index(request):
         'top': Paste.objects.filter(hits__gt=0).order_by('-hits')[:5],
     })
 
+
 @require_GET
 def all_pastes(request):
-    return render(request, 'all_pastes.html', {'pastes': Paste.objects.order_by('-timestamp')})
+    return render(request, 'all_pastes.html', {
+        'pastes': Paste.objects.order_by('-timestamp'),
+    })
+
 
 @require_GET
 def show_paste(request, paste_id):
     p = get_object_or_404(Paste, pk=paste_id)
     return render(request, 'show_paste.html', {'paste':p.view()})
 
+
 @require_GET
 def show_raw(request, paste_id):
     p = get_object_or_404(Paste, pk=paste_id)
     p.view()
     return HttpResponse(p.body, content_type='text/plain; charset=utf-8')
+
 
 @require_http_methods(['GET', 'POST'])
 def make_paste(request):
