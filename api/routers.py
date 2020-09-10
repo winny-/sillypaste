37e79555743c1e0a35a199826fc9edf48bc8e38a
Winston Weinert <winston@ml1.net>
2020-09-10 00:39:42 -0500

    missing changeset

--- api/routers.py
+++ api/routers.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from .views import UserViewSet, PasteViewSet, ExpiryLogViewSet, LanguageViewSet
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'user', UserViewSet)
 router.register(r'paste', PasteViewSet)
 router.register(r'expirylog', ExpiryLogViewSet)
 router.register(r'language', LanguageViewSet)
