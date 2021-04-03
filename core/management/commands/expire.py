a6bc77047549f1df313ebf9a5a559adee28b5810
Winston Weinert <winston@ml1.net>
2021-04-03 16:26:53 -0500

    fix ergonomic issues with the manage.py commands

--- core/management/commands/expire.py
+++ core/management/commands/expire.py
@@ -2,22 +2,26 @@ from django.core.management.base import BaseCommand, CommandError
 from core.models import Paste, ExpiryLog
 from django.utils import timezone
 from django.db.models import Sum
+import textwrap
+
 
 class Command(BaseCommand):
     help = 'Delete expired pastes.'
 
     def handle(self, *args, **options):
-        expired = Paste.objects.filter(expiry__lte=timezone.now())
+        cutoff = timezone.now()
+        print(f'Searching for pastes that expire at or before {cutoff}')
+        expired = Paste.objects.filter(expiry__lte=cutoff)
         expiry_log = ExpiryLog.objects.create()
         expiry_log.save()
         if expired:
             expiry_log.count = expired.count()
             expiry_log.expired_ids = ids = [p.id for p in expired]
             expiry_log.reclaimed_space = expired.aggregate(space=Sum('size'))['space']
-            print('Found {} expired pastes.  IDs={}'.format(
-                len(expired),
-                ','.join(map(str, ids)),
-            ))
+            print(f'Found {len(expired)} pastes:')
+            sp_sep_ids = ' '.join(map(str, ids))
+            block = textwrap.indent(textwrap.fill(sp_sep_ids), ' ' * 4)
+            print(block)
             expired.delete()
             print('Deleted.')
         else:
--- core/management/commands/populate_languages.py
+++ core/management/commands/populate_languages.py
@@ -1,10 +1,18 @@
 from django.core.management.base import BaseCommand, CommandError
 from core.models import Language
 from pygments import lexers
+import sys
 
 
 class Command(BaseCommand):
     help = 'Populate the Language model from pygments lexer data.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--remove-unknown',
+            action='store_true',
+            help='Remove unknown lexers from the database',
+        )
 
     def handle(self, *args, **options):
         names = {lex[0] for lex in lexers.get_all_lexers()}
@@ -12,13 +20,26 @@ class Command(BaseCommand):
         if unknown.exists():
             print('!!! Unknown lexers in database.')
             for u in unknown:
-                print(f'{u.pk:<4} {u.name}')
-            if input('Continue and DELETE y/n? ') != 'y':
-                return
+                print(' ' * 4, f'id={u.pk:<4} name="{u.name}"', sep='')
+            if not options['remove_unknown']:
+                if not sys.stdin.isatty():
+                    print('!!! stdin is not a tty.  Add --remove-unknown to invocation or run interactively.  Aborting.')
+                    return
+                if input('Continue and DELETE y/n? ') != 'y':
+                    return
             unknown.delete()
-        print('Updating database...', end='', flush=True)
+            print('Deleted.', end='\n\n')
+        print('Updating database...')
+        unprinted_names = []
+        width = 0
         for n in names:
-            print(' ', n, sep='', end='', flush=True)
+            unprinted_names.append(n)
+            width += len(n) + 1
             lang = Language.objects.get_or_create(name=n)
-        print()
+            if width > 64:
+                print(' ' * 4, ' '.join(unprinted_names), sep='')
+                width = 0
+                unprinted_names = []
+        if unprinted_names:
+            print(' ' * 4, ' '.join(unprinted_names), sep='')
         print('DONE')
