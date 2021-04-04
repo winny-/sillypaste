301aa83eb1193d0d9f84b4967286b3ef752ed28e
Winston Weinert <winston@ml1.net>
2021-04-03 22:00:39 -0500

    more tests

---
+++ core/validators/tests.py
@@ -0,0 +1,18 @@
+from . import validate_future_datetime
+from datetime import datetime
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+import pytz
+
+
+class TestValidatoFutureDatetime(TestCase):
+
+    def test_past(self):
+        with self.assertRaises(ValidationError):
+            t = datetime(1980, 1, 1, 19, 1, 0, 0, pytz.UTC)
+            validate_future_datetime(timezone.localtime(t))
+
+    def test_future(self):
+        t = datetime(2100, 1, 1, 19, 1, 0, 0, pytz.UTC)
+        self.assertIsNone(validate_future_datetime(timezone.localtime(t)))
--- core/validators/validators.py
+++ core/validators/validators.py
@@ -3,6 +3,9 @@ from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
 
+__all__ = ['validate_future_datetime']
+
+
 def validate_future_datetime(value):
     if value <= timezone.now():
         raise ValidationError(_('%(value)s is in the past'),
