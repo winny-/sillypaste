b24618f3d9a56175fae46b44ea973dccf4cc77e9
Winston Weinert <winston@ml1.net>
2021-11-07 19:03:07 -0600

    Watch for .env changes

--- sillypaste/apps.py
+++ sillypaste/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
+from django.utils.autoreload import autoreload_started
+
+
+def watch_extra_files(sender, **kwargs):
+    """
+    Watch some extra files.
+
+    See https://stackoverflow.com/a/43593959/2720026
+    """
+    sender.watch_dir('.', '.env')
 
 
 class SillyPasteConfig(AppConfig):
     name = 'sillypaste'
+
+    def ready(self):
+        autoreload_started.connect(watch_extra_files)
