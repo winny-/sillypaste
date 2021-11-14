89d715fff7f91f266f2b366a50ea78e2cdb92435
Winston Weinert <winston@ml1.net>
2021-11-14 01:10:02 -0600

    indicate currently selected sort column

---
+++ web/templatetags/sortable_column.py
@@ -0,0 +1,16 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('sortable_column_snippet.html')
+def sortable_column(request, pretty_name, identifier, default=False):
+    current = request.GET.get('sort', identifier if default else None)
+    print(identifier, current)
+    return {
+        'pretty_name': pretty_name,
+        'identifier': identifier,
+        'request': request,
+        'selected': identifier == current,
+    }
