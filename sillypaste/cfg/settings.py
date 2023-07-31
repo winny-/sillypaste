1afe4b02914d15c5c2722526819ac3976df0a6c1
Winston Weinert <git@winny.tech>
2023-07-30 22:54:10 -0500

    fix: set appropriate allowed hosts in local dev env

--- sillypaste/cfg/settings.py
+++ sillypaste/cfg/settings.py
@@ -34,7 +34,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     h.strip()
-    for h in os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
+    for h in os.environ.get(
+        'DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost,::1'
+    ).split(',')
     if h.strip()
 ]
 
